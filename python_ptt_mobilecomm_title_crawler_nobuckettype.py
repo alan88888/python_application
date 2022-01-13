dd8b2250ee013646ee3dc6795fed37a97a48db6d
alan88888 <45704365+alan88888@users.noreply.github.com>
2022-01-13 18:32:27 +0800

    Create python_ptt_mobilecomm_title_crawler_nobuckettype.py

---
+++ python_ptt_mobilecomm_title_crawler_nobuckettype.py
@@ -0,0 +1,28 @@
+# #coding=utf-8
+import requests as re
+from bs4 import BeautifulSoup as bs
+import time 
+import os
+from requests.models import Response
+
+num=1
+with open('test1.txt','w',encoding='utf-8') as t:
+    while num<=100:
+        url = f'https://www.ptt.cc/bbs/MobileComm/index{num}.html'
+        header={'referer': 'https://www.ptt.cc/bbs/index.html','user-agent': 'Mozilla/5.0'}
+        cookies={'over18': '1'}
+        res=re.get(url,headers=header,cookies=cookies)
+        soup=bs(res.text,'html.parser')
+        titles=soup.find_all('div',class_='title')
+        t.write(f'第{num}頁\n')
+        keyword='水桶'
+        for i,title in enumerate(titles):
+            if keyword not in title.a.text :    
+                t.write(f'\t{i+1}')
+                t.write(title.a.text)
+                t.write('\n')
+            else:
+                continue
+        num=num+1
+        time.sleep(1)
+
