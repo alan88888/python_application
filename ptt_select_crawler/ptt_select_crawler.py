45da921b58631f34eb55de6698d63286fa320a23
alan88888 <45704365+alan88888@users.noreply.github.com>
2022-01-14 15:06:37 +0800

    Create ptt_select_crawler.py

---
+++ ptt_select_crawler/ptt_select_crawler.py
@@ -0,0 +1,29 @@
+#coding=utf-8
+import requests as re
+from bs4 import BeautifulSoup as bs
+import time 
+import os
+from requests.models import Response
+snum=eval(input('請輸入想要爬的頁數:'))
+num=1
+filename=input('請輸入想要取的檔名:')
+with open(f'{filename}.txt','w',encoding='utf-8') as t:
+    while num<=snum:
+        url = f'https://www.ptt.cc/bbs/MobileComm/index{num}.html'
+        header={'referer': 'https://www.ptt.cc/bbs/index.html','user-agent': 'Mozilla/5.0'}
+        cookies={'over18': '1'}
+        res=re.get(url,headers=header,cookies=cookies)
+        soup=bs(res.text,'html.parser')
+        titles=soup.find_all('div',class_='title')
+        t.write(f'第{num}頁\n')
+        keyword='水桶'
+        keyword1='公告'
+        for i,title in enumerate(titles):
+            if (keyword and keyword1) not in title.a.text :    
+                #t.write(f'\t{i+1}')
+                t.write(f'\t{title.a.text}')
+                t.write('\n')
+            else:
+                continue
+        num=num+1
+        #time.sleep(1)
